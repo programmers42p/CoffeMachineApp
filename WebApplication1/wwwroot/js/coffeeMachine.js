1fc09e4c0ce57910314ce0f37e367c7ec3432057
programmers42p <71330100+programmers42p@users.noreply.github.com>
2020-11-09 21:03:45 +0300

    Pf

--- WebApplication1/wwwroot/js/coffeeMachine.js
+++ WebApplication1/wwwroot/js/coffeeMachine.js
@@ -15,17 +15,48 @@ class CoffeeMachine {
 }
 
 function customCoffee(){
-    let coffeAmount = document.getElementById("coffeeAmount")
-    coffeAmount.setAttribute("disabled", "true")
+    let coffeAmount = document.getElementById("coffeeAmount");
+    coffeAmount.removeAttribute("disabled");
 
-    let checkCoffee = document.getElementById("checkCoffee")
-    checkCoffee.setAttribute("disabled", "true")
+    let coffeeMilk = document.getElementById("coffeeMilk");
+    coffeeMilk.removeAttribute("disabled");
+
+    let coffeeLemon = document.getElementById("coffeeLemon");
+    coffeeLemon.removeAttribute("disabled");
+
+    let coffeeIce = document.getElementById("coffeeIce");
+    coffeeIce.removeAttribute("disabled");
 
 }
 function specificCoffee(){
-    let coffeAmount = document.getElementById("coffeeAmount")
-    coffeAmount.setAttribute("disabled", "false")
+    let coffeAmount = document.getElementById("coffeeAmount");
+    coffeAmount.setAttribute("disabled", "disabled");
 
-    let checkCoffee = document.getElementById("checkCoffee")
-    checkCoffee.setAttribute("disabled", "false")
+    let coffeeMilk = document.getElementById("coffeeMilk");
+    coffeeMilk.setAttribute("disabled", "disabled");
+
+    let coffeeIce = document.getElementById("coffeeIce");
+    coffeeIce.setAttribute("disabled", "disabled");
+
+    let coffeeLemon = document.getElementById("coffeeLemon");
+    coffeeLemon.setAttribute("disabled", "disabled");
+
+    c
 }
+let espresso = document.getElementById("espresso");
+espresso.onclick = specificCoffee;
+
+let custom = document.getElementById("custom");
+custom.onclick = customCoffee;
+
+let cappuccino = document.getElementById("cappuccino");
+cappuccino.onclick = specificCoffee;
+
+let americano = document.getElementById("americano");
+americano.onclick = specificCoffee;
+
+let latte = document.getElementById("latte");
+latte.onclick = specificCoffee;
+
+let frappe = document.getElementById("frappe");
+frappe.onclick = specificCoffee;
